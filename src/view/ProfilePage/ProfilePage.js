a1be8f0696e2fe550edb1636c21e844ff3626db1
Albert Vatafu <60220400+AbtVtf@users.noreply.github.com>
2022-09-22 22:11:27 +0300

    fixed redirects

--- src/view/ProfilePage/ProfilePage.js
+++ src/view/ProfilePage/ProfilePage.js
@@ -59,25 +59,28 @@ const ProfilePage = () => {
   };
   return (
     <div className="page-profile-container">
-      <p className="profile-page-title">Saved Articles</p>
       {titles.length > 0 ? (
-        titles?.map((item, index) => {
-          return (
-            <div
-              className="component-card-wrapper"
-              key={index}
-              onClick={() => {
-                handleArticle(item["id_title"]);
-              }}
-            >
-              <NewsCard
-                index={index + 1}
-                title={item.title}
-                category={item.category}
-              />
-            </div>
-          );
-        })
+        <>
+          <p className="profile-page-title">Saved Articles</p>
+
+          {titles?.map((item, index) => {
+            return (
+              <div
+                className="component-card-wrapper"
+                key={index}
+                onClick={() => {
+                  handleArticle(item["id_title"]);
+                }}
+              >
+                <NewsCard
+                  index={index + 1}
+                  title={item.title}
+                  category={item.category}
+                />
+              </div>
+            );
+          })}
+        </>
       ) : (
         <>
           <p className="profile-page-title">You should go read some news</p>
